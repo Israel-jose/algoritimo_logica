7cbd1a05fd01dcf76c6f290db91f6f726d732328
Israel José <israelpilotto@gmail.com>
2026-09-03 17:28:34 -0300

    programa que calcula uma soma dos impares entre dois números
    o programa pergunta dois números ao usuário e soma os impares entre eles;
    
    não importa a ordem dos números;

---
+++ visualg/soma_impares.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class soma_impares {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x, soma = 0,  y, troca;
+			
+		
+		System.out.println("Dgite dois numeros: ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		if(x > y) {
+			troca = x;
+			x = y;
+			y = troca;
+		}
+		for(int i = x + 1; i < y; i++) {
+			if(i % 2 == 1) {
+			soma = soma + i;
+			}
+		}
+		
+		System.out.println("SOMA DOS IMPARES: " + soma);
+		
+		sc.close();
+	}
+
+}
