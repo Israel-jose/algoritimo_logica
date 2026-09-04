fdf9c5d5667cdbd517b76f08933871c89edaa347
Israel José <israelpilotto@gmail.com>
2026-09-04 20:07:16 -0300

    exercício que soma os números da linha da matriz e armazena em um vetor

---
+++ ws - eclipse/soma_linhas.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class soma_linhas {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual a quantidade de linhas da matriz? ");
+		int m = sc.nextInt();
+		
+		System.out.print("Qual a quantidade de colunas da matriz? ");
+		int n = sc.nextInt();
+		
+		double[][] mat = new double[m][n];
+		double[] vet = new double[m];
+		
+		for(int i = 0; i < m; i++) {
+			System.out.println("Digite os elementos da " + (i+1) + "a. linha: ");
+			for(int j = 0; j < n; j++) {
+				mat [i][j] = sc.nextDouble();
+			}
+		}
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				vet[i] = vet[i] + mat[i][j];
+			}
+		}
+		System.out.println("VETOR GERADO: ");
+		for(int i = 0; i < m; i++){
+			System.out.println(vet[i]);
+		}
+		
+		
+		sc.close();
+	}
+
+}
